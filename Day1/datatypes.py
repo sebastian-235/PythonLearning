63310a7e0a6daaf3547c6b9354fb02843e64e0b5
Sebastian Mariya Gnanam <sebastian.mg@plintron.com>
2024-03-05 19:07:36 +0530

    Initial commit

---
+++ Day1/datatypes.py
@@ -0,0 +1,14 @@
+x = 10
+y = 10.0000
+seb = "welcome"
+stian = 'Testing'
+boolean = True
+boolean1 = False
+
+print(x,type(x))
+print(type(y))
+print(type(seb))
+print(type(boolean))
+print(type(boolean1))
+x = 'hello'
+print(type(x))
---
+++ Day1/variables.py
@@ -0,0 +1,33 @@
+x = 10
+y = 20
+print (x)
+print(y)
+
+### Example 2
+
+x = 10
+y = 20
+print (x,y)
+
+### Example 3
+
+x,y,z = 10,20,'welcome'
+print (x,y,z)
+
+
+###Example 4
+
+x=y=z=10
+a=10,12,13
+print(x,y,z)
+print(a)
+
+### Example 5
+
+print("Swapping")
+x = 10
+y = 20
+print (x,y)
+# y,x=x,y
+x,y=y,x
+print(x,y)
